c624e9e617fde762816cac823b8edb7bc008009b
Anna Karachkovska <akarachkovska@gmail.com>
2022-12-15 11:48:34 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <DateProvider>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/command-project-react">
+          <BrowserRouter basename="/slim-mom">
             <App />
           </BrowserRouter>
         </PersistGate>
